26ae81d97b04235b9e1831ea83408bae6cf53a63
Katios <kate.chonkina@gmail.com>
2022-04-06 17:08:23 +0600

    Initial Commit

---
+++ MultipleActionSheets/MultipleActionSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleActionSheetsApp.swift
+//  MultipleActionSheets
+//
+//  Created by Екатерина Чернова on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultipleActionSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
